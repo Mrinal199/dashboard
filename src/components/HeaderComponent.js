7f2f1a827639c09f3e1bdb697bc781839eb8c65f
Mrinal199 <mrinalsuman11@gmail.com>
2024-02-28 12:03:52 +0530

    first commitas569

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
         width: 35,
         borderRadius: 50,
         marginLeft: 14,
-        border: '1px solid #DFE0EB',
+        border: '1px solid red',
     },
     container: {
         height: 40
@@ -24,48 +24,99 @@ const styles = StyleSheet.create({
         fontStyle: 'normal',
         fontWeight: 600,
         fontSize: 14,
+        color: 'white',
         lineHeight: '20px',
         textAlign: 'right',
-        letterSpacing: 0.2
+        letterSpacing: 0.2,
+        '@media (max-width: 768px)': {
+            display: 'none'
+        }
     },
     separator: {
         borderLeft: '1px solid #DFE0EB',
         marginLeft: 32,
         marginRight: 32,
         height: 32,
-        width: 2
+        width: 2,
+        '@media (max-width: 768px)': {
+            marginLeft: 12,
+            marginRight: 12
+        }
     },
     title: {
         fontFamily: 'Muli',
         fontStyle: 'normal',
         fontWeight: 'bold',
+        color: 'white',
         fontSize: 24,
         lineHeight: '30px',
-        letterSpacing: 0.3
+        letterSpacing: 0.3,
+        '@media (max-width: 768px)': {
+            marginLeft: 36
+        },
+        '@media (max-width: 468px)': {
+            fontSize: 20
+        }
+    },
+    iconStyles: {
+        cursor: 'pointer',
+        marginLeft: 25,
+        '@media (max-width: 768px)': {
+            marginLeft: 12
+        }
+    },
+    searchContainer: {
+        position: 'relative'
+    },
+    searchInput: {
+        width: 200,
+        padding: '8px 30px 8px 10px',
+        borderRadius: 20,
+        border: '1px solid #ccc',
+        ':focus': {
+            outline: 'none',
+            borderColor: '#007bff'
+        }
+    },
+    searchIcon: {
+        position: 'absolute',
+        right: 10,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        fontSize: 20,
+        cursor: 'pointer',
+        color: '#777'
+    },
+    backgroundImage: {
+        backgroundImage: 'url(\'https://itchronicles.com/wp-content/uploads/2020/11/where-is-ai-used.jpg\')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
     }
 });
 
 function HeaderComponent(props) {
     const { icon, title, ...otherProps } = props;
     return (
-    <div>
-        <Row vertical="center">
-                    <span className={css(styles.name, styles.cursorPointer)}>Mrinal Suman</span>
-                    <img src="https://scontent.fpat1-2.fna.fbcdn.net/v/t39.30808-6/334217607_2279336932272158_108377661778100570_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=BHjsusPiX9QAX-DQBRH&_nc_ht=scontent.fpat1-2.fna&oh=00_AfAEy6R9zSioZzxw6oMA44pyT5Fy1UFwI2ZesWzfF2wVRw&oe=65E2B40A" alt="avatar" className={css(styles.avatar, styles.cursorPointer)} />
-        </Row><br></br><br></br><br></br>
-        <Row className={css(styles.container)} vertical="center" horizontal="space-between" {...otherProps}>
-            <span className={css(styles.title)}>{title}</span>
+        <div className={css(styles.backgroundImage)}>
             <Row vertical="center">
-                <div className={css(styles.cursorPointer)}>
-                    <IconSearch />
-                </div>
-                <div style={{ marginLeft: 25 }} className={css(styles.cursorPointer)}>
-                    <IconBellNew />
-                </div>
-                
+                    <span className={css(styles.name, styles.cursorPointer)}>Mrinal Suman</span>
+                    <img src="https://th.bing.com/th/id/OIP.HZ9w8YiyROitCGC910VXAQAAAA?rs=1&pid=ImgDetMain" alt="avatar" className={css(styles.avatar, styles.cursorPointer)} />
+            </Row> <br></br><br></br><br></br>
+            <Row className={css(styles.container)} vertical="center" horizontal="space-between" {...otherProps}>
+                <span className={css(styles.title)}>{title}</span>
+                <Row vertical="center" className={css(styles.searchContainer)}>
+                    <input type="text" placeholder="Search..." className={css(styles.searchInput)} />
+                    <div className={css(styles.searchIcon)}>
+                        <IconSearch />
+                    </div>
+                </Row>
+                <Row vertical="center">
+                    <div className={css(styles.iconStyles)}>
+                        <IconBellNew />
+                    </div>
+                </Row>
             </Row>
-        </Row>
-    </div>
+        </div>
     );
 }
 
